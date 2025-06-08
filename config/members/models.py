44edef85cc099ba235d68ab53f9e32fe8a305d76
Hassana207 <hassan.ayinde@dcmail.ca>
2025-06-08 16:48:29 -0400

    Create user registration form by extending UserCreationForm and saving to built-in User model

--- config/members/models.py
+++ config/members/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+
+from django import forms
+from django.contrib.auth.models import  User 
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your models here.
+
+class UserRegistration(UserCreationForm):
+    class Meta :
+        model = User 
+        fields = ['username','password1','password2']
+    
+
+
